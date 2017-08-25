b434279d80720c31097a71c339020e0b2d26abac
The Hulk <q>
2017-08-25 09:49:28 +0800

    first

---
+++ README.md
@@ -0,0 +1,1 @@
+# prophecyun.github.io
---
+++ app/mainoperator.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var platform_browser_dynamic_1 = require("@angular/platform-browser-dynamic");
+var operator_module_1 = require("./operator/operator.module");
+platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(operator_module_1.OperatorModule);
+//# sourceMappingURL=mainoperator.js.map
