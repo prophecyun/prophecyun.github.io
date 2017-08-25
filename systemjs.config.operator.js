e7e77a20da50f7f9aa43005d0335d2c87a0cb53c
The Hulk <q>
2017-08-25 11:35:02 +0800

    s

--- systemjs.config.operator.js
+++ systemjs.config.operator.js
@@ -6,7 +6,7 @@
   System.config({
     paths: {
       // paths serve as alias
-      'npm:': 'js/'
+      'npm:': 'node_modules/'
     },
     // map tells the System loader where to look for things
     map: {
@@ -14,25 +14,32 @@
       app: 'app',
 
       // angular bundles
-      '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
-      '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
-      '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
-      '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
-      '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
-      '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
-      '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
-      '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
-      '@angular/upgrade': 'npm:@angular/upgrade/bundles/upgrade.umd.js',
+      '@angular/core': 'js/@angular/core/bundles/core.umd.js',
+      '@angular/common': 'js/@angular/common/bundles/common.umd.js',
+      '@angular/compiler': 'js/@angular/compiler/bundles/compiler.umd.js',
+      '@angular/platform-browser': 'js/@angular/platform-browser/bundles/platform-browser.umd.js',
+      '@angular/platform-browser-dynamic': 'js/@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
+      '@angular/http': 'js/@angular/http/bundles/http.umd.js',
+      // '@angular/router': 'js/@angular/router/bundles/router.umd.js',
+      '@angular/forms': 'js/@angular/forms/bundles/forms.umd.js',
+      // '@angular/upgrade': 'js/@angular/upgrade/bundles/upgrade.umd.js',
       // '@angular/material': 'npm:@angular/material/material.umd.js',
 
+  
+        'rxjs':                       'js/rxjs',
+        // 'angular2-in-memory-web-api': 'js/angular2-in-memory-web-api',
+        // '@angular':                   'js/@angular',
+  
+    
+
       // other libraries
-      'rxjs': 'npm:rxjs',
-      'angular-in-memory-web-api': 'npm:angular-in-memory-web-api',
+      // 'rxjs': 'npm:rxjs',
+      // 'angular-in-memory-web-api': 'npm:angular-in-memory-web-api',
       // 'ng2-dragula': 'npm:ng2-dragula',
       // 'dragula': 'npm:dragula',
-      'contra': 'npm:contra',
-      'crossvent': 'npm:crossvent',
-      'atoa': 'npm:atoa',
+      // 'contra': 'npm:contra',
+      // 'crossvent': 'npm:crossvent',
+      // 'atoa': 'npm:atoa',
       // 'ticky': 'npm:ticky',
     },
     // packages tells the System loader how to load when no filename and/or no extension
@@ -47,31 +54,31 @@
       'angular-in-memory-web-api': {
         main: './index.js',
         defaultExtension: 'js'
-      },
-      'ng2-dragula': {
-        main: './ng2-dragula.js',
-        defaultExtension: 'js'
-      },
-      'dragula': {
-        main: './dragula.js',
-        defaultExtension: 'js'
-      },
-      'contra': {
-        main: './contra.js',
-        defaultExtension: 'js'
-      },
-      'crossvent': {
-        main: './dist/crossvent.min.js',
-        defaultExtension: 'js'
-      },
-      'atoa': {
-        main: './atoa.js',
-        defaultExtension: 'js'
-      },
-      'ticky': {
-        main: './ticky.js',
-        defaultExtension: 'js'
-      },
+      }
+      // 'ng2-dragula': {
+      //   main: './ng2-dragula.js',
+      //   defaultExtension: 'js'
+      // },
+      // 'dragula': {
+      //   main: './dragula.js',
+      //   defaultExtension: 'js'
+      // },
+      // 'contra': {
+      //   main: './contra.js',
+      //   defaultExtension: 'js'
+      // },
+      // 'crossvent': {
+      //   main: './dist/crossvent.min.js',
+      //   defaultExtension: 'js'
+      // },
+      // 'atoa': {
+      //   main: './atoa.js',
+      //   defaultExtension: 'js'
+      // },
+      // 'ticky': {
+      //   main: './ticky.js',
+      //   defaultExtension: 'js'
+      // },
     }
   });
 })(this);
